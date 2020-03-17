26d041488e682268fbb02a92ef950b70e29022a3
Bryan Ramos <b.r.tech95@gmail.com>
2020-03-17 07:09:13 -0400

    build

---
+++ build/precache-manifest.1905a41d2dbf6e2c76df55777b1a9658.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2700e8b149d410f40ba2270a3d6203a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b639a2f1294c75220220",
+    "url": "/static/css/main.37d4ccaf.chunk.css"
+  },
+  {
+    "revision": "f262f3ebe6632e5d88c6",
+    "url": "/static/js/2.80d00b8e.chunk.js"
+  },
+  {
+    "revision": "7d66cbb52292c6d86ff795a004c53661",
+    "url": "/static/js/2.80d00b8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b639a2f1294c75220220",
+    "url": "/static/js/main.dc2ff5c2.chunk.js"
+  },
+  {
+    "revision": "f81843e7a44adee21a59",
+    "url": "/static/js/runtime-main.8ada3417.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
